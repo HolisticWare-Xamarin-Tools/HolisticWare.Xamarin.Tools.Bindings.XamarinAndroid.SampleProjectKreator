5b3239179f1ab507f9abf670fbafb8f41114cdfe
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-12-24 23:43:43 +0100

    1st sample version of creating/loading and biuilding project

---
+++ samples/Bindings.Sample.Project.Kreator.App.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using HolisticWare.Xamarin.Tools.Bindings.XamarinAndroid.SampleProjectKreator.Core;
+
+namespace Bindings.Sample.Project.Kreator.App.Console
+{
+    class Program
+    {
+        static string path =
+            // "/Projects/hw-tools/Kreator/source/HolisticWare.Xamarin.Tools.Bindings.XamarinAndroid.SampleProjectKreator.Core/HolisticWare.Xamarin.Tools.Bindings.XamarinAndroid.SampleProjectKreator.Core.csproj"
+            // "/Projects/hw-tools/Kreator/samples/Bindings.Sample.Project.Kreator.App.Console/Bindings.Sample.Project.Kreator.App.Console.csproj"
+            "/Projects/hw-tools/Kreator/samples/App.XamarinAndroid/App.XamarinAndroid.csproj"
+            ;
+
+        static void Main(string[] args)
+        {
+            ProjectKreator pk = new ProjectKreator();
+            pk.Load(path);
+
+            return;
+        }
+    }
+}
