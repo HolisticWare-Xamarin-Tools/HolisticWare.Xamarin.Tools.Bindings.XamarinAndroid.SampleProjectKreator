ac9c378ca174b2da7f784dbd1e013a0005a1b15c
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-12-26 22:52:17 +0100

    options refactoring

--- samples/Bindings.Sample.Project.Kreator.App.Console/Program.cs
+++ samples/Bindings.Sample.Project.Kreator.App.Console/Program.cs
@@ -6,67 +6,41 @@ namespace Bindings.Sample.Project.Kreator.App
 {
     class Program
     {
+        static bool show_help = false;
+        static int verbosity = 0;
+
         static string path =
             // "/Projects/hw-tools/Kreator/source/HolisticWare.Xamarin.Tools.Bindings.XamarinAndroid.SampleProjectKreator.Core/HolisticWare.Xamarin.Tools.Bindings.XamarinAndroid.SampleProjectKreator.Core.csproj"
             // "/Projects/hw-tools/Kreator/samples/Bindings.Sample.Project.Kreator.App.Console/Bindings.Sample.Project.Kreator.App.Console.csproj"
             "/Projects/hw-tools/SampleProjectKreator/samples/App.XamarinAndroid/App.XamarinAndroid.csproj"
             ;
 
-        static string folder_input_android = null;
-        static string folder_input_dotnet = null;
-        static bool show_help = false;
-        static int verbosity = 0;
+        static ProjectKreator project_kreator = null;
 
         static void Main(string[] args)
         {
-            Mono.Options.OptionSet option_set = new Mono.Options.OptionSet()
-            {
-                // ../../../../../../../X/AndroidSupportComponents-AndroidX-binderate/output/AndroidSupport.api-diff.xml
-                {
-                    "i|input=",
-                    "input folder with Android Support Diff (Metadata.xml, Metadata*.xml)",
-                    (string v) =>
-                    {
-                        folder_input_android = v;
-                        return;
-                    }
-                },
-                {
-                    "o|output=",
-                    "output folder with AndroidX Xamarin.Android Metadata files (Metadata.xml, Metadata*.xml)",
-                    (string v) =>
-                    {
-                        folder_input_dotnet = v;
-                        return;
-                    }
-                },
-                {
-                    "h|help",
-                    "show this message and exit",
-                    v => show_help = v != null
-                },
-            };
+            project_kreator = new ProjectKreator();
 
             List<string> extra;
             try
             {
-                extra = option_set.Parse(args);
+                extra = project_kreator.OptionSet.Parse(args);
             }
             catch (Mono.Options.OptionException e)
             {
                 Console.Write("XamarinAndroid.ProjectKreator: ");
                 Console.WriteLine(e.Message);
-                Console.WriteLine("Try `AndroidX.Migraineator --help' for more information.");
+                Console.WriteLine("Try `XamarinAndroid.ProjectKreator --help' for more information.");
                 return;
             }
 
             if (show_help)
             {
-                ShowHelp(option_set);
+                ShowHelp(project_kreator.OptionSet);
                 return;
             }
 
-            string message = "AndroidX.Migraineator";
+            string message = "XamarinAndroid.ProjectKreator";
             if (extra.Count > 0)
             {
                 message = string.Join(" ", extra.ToArray());
@@ -76,33 +50,10 @@ namespace Bindings.Sample.Project.Kreator.App
             {
                 Debug($"Using default message: {message}");
             }
-            ProjectKreator pk = new ProjectKreator();
-            pk.Load(path);
+            project_kreator.Load(path);
 
             return;
         }
 
-        static void Debug(string format, params object[] args)
-        {
-            if (verbosity > 0)
-            {
-                Console.Write("# ");
-                Console.WriteLine(format, args);
-            }
-
-            return;
-        }
-
-        static void ShowHelp(Mono.Options.OptionSet p)
-        {
-            Console.WriteLine("Usage: greet [OPTIONS]+ message");
-            Console.WriteLine("Greet a list of individuals with an optional message.");
-            Console.WriteLine("If no message is specified, a generic greeting is used.");
-            Console.WriteLine();
-            Console.WriteLine("Options:");
-            p.WriteOptionDescriptions(Console.Out);
-
-            return;
-        }
     }
 }
