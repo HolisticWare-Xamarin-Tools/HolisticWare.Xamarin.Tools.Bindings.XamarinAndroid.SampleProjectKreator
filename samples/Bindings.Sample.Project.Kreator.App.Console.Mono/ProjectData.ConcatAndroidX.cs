2a3a1e5cda1d151e9f78b67be2b2b0199272b3b6
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2019-02-21 23:31:09 +0100

    sample

--- samples/Bindings.Sample.Project.Kreator.App.Console.Mono/ProjectData.ConcatAndroidX.cs
+++ samples/Bindings.Sample.Project.Kreator.App.Console.Mono/ProjectData.ConcatAndroidX.cs
@@ -20,86 +20,86 @@ namespace Bindings.Sample.Project.Kreator.App.Console.Mono
                     {
                         (
                             GroupName : "Material",
-                            FeatureName : "Material-diverse",
-                            ProjectName : "topeka-base",
+                            FeatureName : "Material",   // Folder
+                            ProjectName : "diverse",    // SubFolder
                             InputFolderPath : $"../../output/repos/Material-diverse/topeka/base",
-                            OutputFolderPath : $"/Projects/tmp/Xamarin.Android.Samples/output/Material-diverse/topeka-base/"
+                            OutputFolderPath : $"/Projects/tmp/Xamarin.Android.Samples/output/Material/diverse/topeka-base/"
                         ),
                         (
                             GroupName : "Material",
-                            FeatureName : "Material-diverse",
-                            ProjectName : "topeka-quiz",
-                            InputFolderPath : $"/output/repos/Material-diverse/topeka/quiz",
-                            OutputFolderPath : $"/Projects/tmp/Xamarin.Android.Samples/output/Material-diverse/topeka-quiz/"
+                            FeatureName : "Material",   // Folder
+                            ProjectName : "diverse",    // SubFolder
+                            InputFolderPath : $"../../output/repos/Material-diverse/topeka/quiz",
+                            OutputFolderPath : $"/Projects/tmp/Xamarin.Android.Samples/output/Material/diverse/topeka-quiz/"
                         ),
                         (
                             GroupName : "Material",
-                            FeatureName : "Material-diverse",
-                            ProjectName : "topeka-categories",
-                            InputFolderPath : $"/output/repos/Material-diverse/topeka/categories",
+                            FeatureName : "Material",   // Folder
+                            ProjectName : "diverse",    // SubFolder
+                            InputFolderPath : $"../../output/repos/Material-diverse/topeka/categories",
                             OutputFolderPath : $"/Projects/tmp/Xamarin.Android.Samples/output/Material-diverse/topeka-categories/"
                         ),
                         (
                             GroupName : "Material",
-                            FeatureName : "Material-diverse",
-                            ProjectName : "design-library-app",
-                            InputFolderPath : $"/output/repos/Material-diverse/design-library/app",
+                            FeatureName : "Material",   // Folder
+                            ProjectName : "diverse",    // SubFolder
+                            InputFolderPath : $"../../output/repos/Material-diverse/design-library/app",
                             OutputFolderPath : $"/Projects/tmp/Xamarin.Android.Samples/output/Material-diverse/design-library/app/"
                         ),
                         (
                             GroupName : "Material",
-                            FeatureName : "Material-diverse",
-                            ProjectName : "design-library-1-Base",
-                            InputFolderPath : $"/output/repos/Material-diverse/design-library/1-Base",
+                            FeatureName : "Material",   // Folder
+                            ProjectName : "design-library",
+                            InputFolderPath : $"../../output/repos/Material-diverse/design-library/1-Base",
                             OutputFolderPath : $"/Projects/tmp/Xamarin.Android.Samples/output/Material-diverse/design-library/1-Base/"
                         ),
                         (
                             GroupName : "Material",
-                            FeatureName : "Material-diverse",
-                            ProjectName : "design-library-2-Color",
-                            InputFolderPath : $"/output/repos/Material-diverse/design-library/2-Color",
+                            FeatureName : "Material",   // Folder
+                            ProjectName : "design-library",
+                            InputFolderPath : $"../../output/repos/Material-diverse/design-library/2-Color",
                             OutputFolderPath : $"/Projects/tmp/Xamarin.Android.Samples/output/Material-diverse/design-library/2-Color"
                         ),
                         (
                             GroupName : "Material",
-                            FeatureName : "Material-diverse",
-                            ProjectName : "design-library-3-Layout",
-                            InputFolderPath : $"/output/repos/Material-diverse/design-library/3-Layout",
+                            FeatureName : "Material",   // Folder
+                            ProjectName : "design-library",
+                            InputFolderPath : $"../../output/repos/Material-diverse/design-library/3-Layout",
                             OutputFolderPath : $"/Projects/tmp/Xamarin.Android.Samples/output/Material-diverse/design-library/3-Layout"
                         ),
                         (
                             GroupName : "Material",
-                            FeatureName : "Material-diverse",
-                            ProjectName : "design-library-4-RecyclerView",
-                            InputFolderPath : $"/output/repos/Material-diverse/design-library/4-RecyclerView",
+                            FeatureName : "Material",   // Folder
+                            ProjectName : "design-library",
+                            InputFolderPath : $"../../output/repos/Material-diverse/design-library/4-RecyclerView",
                             OutputFolderPath : $"/Projects/tmp/Xamarin.Android.Samples/output/Material-diverse/design-library/4-RecyclerView"
                         ),
                         (
                             GroupName : "Material",
-                            FeatureName : "Material-diverse",
-                            ProjectName : "design-library-5-VectorAssets",
-                            InputFolderPath : $"/output/repos/Material-diverse/design-library/5-VectorAssets",
+                            FeatureName : "Material",   // Folder
+                            ProjectName : "design-library",
+                            InputFolderPath : $"../../output/repos/Material-diverse/design-library/5-VectorAssets",
                             OutputFolderPath : $"/Projects/tmp/Xamarin.Android.Samples/output/Material-diverse/design-library/5-VectorAssets"
                         ),
                         (
                             GroupName : "Material",
                             FeatureName : "Material-diverse",
                             ProjectName : "design-library-6-PageElement",
-                            InputFolderPath : $"/output/repos/Material-diverse/design-library/6-PageElement",
+                            InputFolderPath : $"../../output/repos/Material-diverse/design-library/6-PageElement",
                             OutputFolderPath : $"/Projects/tmp/Xamarin.Android.Samples/output/Material-diverse/design-library/6-PageElement"
                         ),
                         (
                             GroupName : "Material",
-                            FeatureName : "Material-diverse",
-                            ProjectName : "design-library-6-PageElement",
-                            InputFolderPath : $"/output/repos/Material-diverse/design-library/6-PageElement",
+                            FeatureName : "Material",   // Folder
+                            ProjectName : "design-library",
+                            InputFolderPath : $"../../output/repos/Material-diverse/design-library/6-PageElement",
                             OutputFolderPath : $"/Projects/tmp/Xamarin.Android.Samples/output/Material-diverse/design-library/6-PageElement"
                         ),
                         (
                             GroupName : "Material",
-                            FeatureName : "Material-diverse",
-                            ProjectName : "HackerNewsReader",
-                            InputFolderPath : $"../output/repos/Material-diverse/HackerNewsReader",
+                            FeatureName : "Material",   // Folder
+                            ProjectName : "design-library",
+                            InputFolderPath : $"../../output/repos/Material-diverse/HackerNewsReader",
                             OutputFolderPath : $"/Projects/tmp/Xamarin.Android.Samples/output/Material-diverse/HackerNewsReader/app"
                         ),
 
@@ -107,32 +107,32 @@ namespace Bindings.Sample.Project.Kreator.App.Console.Mono
 
 
                         (
-                            GroupName : "AndroidX.Jetpack",
+                            GroupName : "AndroidX-JetPack",
                             FeatureName : "Java",
                             ProjectName : "JetPack-Navigation",
-                            InputFolderPath : $"/output/repos/AndroidX.Jetpack/JetPack-Navigation/app",
-                            OutputFolderPath : $"/Projects/tmp/Xamarin.Android.Samples/output/AndroidX.Jetpack/JetPack-Navigation/app"
+                            InputFolderPath : $"../../output/repos/AndroidX-JetPack-java/JetPack-Navigation/app",
+                            OutputFolderPath : $"/Projects/tmp/Xamarin.Android.Samples/output/AndroidX-JetPack/Java/JetPack-Navigation/app"
                         ),
                         (
-                            GroupName : "AndroidX.Jetpack",
+                            GroupName : "AndroidX-JetPack",
                             FeatureName : "Java",
                             ProjectName : "Material2",
-                            InputFolderPath : $"/output/repos/AndroidX.Jetpack/Material2",
-                            OutputFolderPath : $"/Projects/tmp/Xamarin.Android.Samples/output/AndroidX.Jetpack/JMaterial2/app"
+                            InputFolderPath : $"../../output/repos/AndroidX-JetPack-java/Material2",
+                            OutputFolderPath : $"/Projects/tmp/Xamarin.Android.Samples/output/AndroidX-JetPack/Java/JMaterial2/app"
                         ),
                         (
-                            GroupName : "AndroidX.Jetpack",
+                            GroupName : "AndroidX-JetPack",
                             FeatureName : "Java",
                             ProjectName : "WAX-Trade-Client",
-                            InputFolderPath : $"/output/repos/AndroidX.Jetpack/WAX-Trade-Client",
-                            OutputFolderPath : $"/Projects/tmp/Xamarin.Android.Samples/output/AndroidX.Jetpack/WAX-Trade-Client/app"
+                            InputFolderPath : $"../../output/repos/AndroidX-JetPack-java/WAX-Trade-Client",
+                            OutputFolderPath : $"/Projects/tmp/Xamarin.Android.Samples/output/AndroidX-JetPack/Java/WAX-Trade-Client/app"
                         ),
                         (
-                            GroupName : "AndroidX.Jetpack",
+                            GroupName : "AndroidX-JetPack",
                             FeatureName : "Java",
                             ProjectName : "Deadline",
-                            InputFolderPath : $"/output/repos/AndroidX.Jetpack/Deadline",
-                            OutputFolderPath : $"/Projects/tmp/Xamarin.Android.Samples/output/AndroidX.Jetpack/Deadline/app"
+                            InputFolderPath : $"../../output/repos/AndroidX-JetPack-java/Deadline",
+                            OutputFolderPath : $"/Projects/tmp/Xamarin.Android.Samples/output/AndroidX-JetPack/Java/Deadline/app"
                         ),
 
                     }
